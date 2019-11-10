6c4decb9fd1033c308f35ecec4e8b5a33fcf9b57
Bohdan <universalshon@gmail.com>
2019-11-11 00:20:49 +0200

    Added some usefull stuff

--- NetCore.Angular.WeatherWidget/NetCore.Angular.WeatherWidget/ClientApp/src/app/app.component.ts
+++ NetCore.Angular.WeatherWidget/NetCore.Angular.WeatherWidget/ClientApp/src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [WeatherService]
 })
 export class AppComponent {
     title = 'Weather Widget';
     latitude = 0;
     longitude = 0;
+
+    constructor(private _weatherService: WeatherService) {
+
+    }
+
+    async GetWeatherByClick(coordinates) {
+        var latitude = coordinates.lat;
+        var longitude = coordinates.lng;
+        var result =  (await this._weatherService.GetWeatherByCoordinates(latitude, longitude)).subscribe((response: any) => {
+            var weatherResponse = response;
+        })
+    }
+
+
 }
---
+++ NetCore.Angular.WeatherWidget/NetCore.Angular.WeatherWidget/Controllers/WeatherController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore.Angular.WeatherWidget.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetWeatherByCoordinates(double latitude, double longitude)
+        {
+            //TODO add coordinates transform from google to absolute format
+            using(var client = new HttpClient())
+            {
+                var url = new Uri("https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139");
+                var result = await client.GetAsync(url);
+                if(result.IsSuccessStatusCode)
+                {
+                    return Ok(await result.Content.ReadAsStringAsync());
+                }
+                else
+                {
+                    return BadRequest(result);
+                }
+            }
+        }
+
+    }
+}
